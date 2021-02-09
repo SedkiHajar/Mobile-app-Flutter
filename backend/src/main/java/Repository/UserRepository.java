622e002689d774cb542be98080762fc35b1e1824
imane elharchaoui <46714956+Imaneelharchaoui@users.noreply.github.com>
2021-02-09 23:33:42 +0000

    Update UserRepository.java

--- backend/src/main/java/Repository/UserRepository.java
+++ backend/src/main/java/Repository/UserRepository.java
@@ -4,12 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devrobot.springbootecommerce.model.User;
 
-/**
- * The Interface UserRepository.
- *
- * @author devrobot
- * @version 1.0
- */
+
 public interface UserRepository extends JpaRepository<User, String> {
 
 }
