bcf76fb16b45f48bd7f366519f71ca5087b5a9a0
imane elharchaoui <46714956+Imaneelharchaoui@users.noreply.github.com>
2021-02-09 23:36:58 +0000

    Update CategoryResource.java

--- backend/src/main/java/resources/CategoryResource.java
+++ backend/src/main/java/resources/CategoryResource.java
@@ -17,12 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devrobot.springbootecommerce.model.Category;
 import com.devrobot.springbootecommerce.repository.CategoryRepository;
 
-/**
- * The Class CategoryResource.
- *
- * @author devrobot
- * @version 1.0
- */
 @RestController
 @RequestMapping("/api/categories")
 @CrossOrigin("*")
@@ -32,53 +26,29 @@ public class CategoryResource {
 	@Autowired
 	private CategoryRepository categoryRepository;
 
-	/**
-	 * Gets all categories.
-	 *
-	 * @return all categories
-	 */
+	
 	@GetMapping(value = "/all")
 	public List<Category> getAll() {
 		return categoryRepository.findAll();
 	}
-	/**
-	 * Gets category.
-	 *
-	 * @return category if exists
-	 */
+	
 	@GetMapping(value = "/get")
 	public Category get(@RequestParam("id") int id) {
 		return categoryRepository.findById(id).get();
 	}
-	/**
-	 * Persist category.
-	 *
-	 * @param category the category
-	 * @return the list
-	 */
+	
 	@PostMapping(value = "/add")
 	public List<Category> persist(@RequestBody final Category category) {
 		categoryRepository.save(category);
 		return categoryRepository.findAll();
 	}
-	/**
-	 * Delete category.
-	 *
-	 * @param id the id
-	 * @return all categories
-	 */
+	
 	@DeleteMapping(value = "/delete")
 	public List<Category> delete(@PathVariable int id) {
 		categoryRepository.deleteById(id);
 		return categoryRepository.findAll();
 	}
-	/**
-	 * Put category.
-	 *
-	 * @param id 		the id
-	 * @param category  the category
-	 * @return all categories
-	 */
+	
 	@PutMapping(value = "/put/{id}")
 	public List<Category> put(@PathVariable int id, @RequestBody Category category) {
 		if (categoryRepository.existsById(id)) {
